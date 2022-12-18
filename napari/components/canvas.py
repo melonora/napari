177594c77523e46fb0de396c3f559a399af0baaf
wmv_hpomen <w-mv@hotmail.com>
2022-12-18 23:17:24 +0100

    Changed color to proper type and added fields

--- napari/components/canvas.py
+++ napari/components/canvas.py
@@ -1,9 +1,13 @@
-from typing import Tuple
+from typing import Optional, Tuple
+
+import numpy as np
 
 from napari.utils.events import EventedModel
 
 
 class Canvas(EventedModel):
+    status: str = ""
+    mouse_over_canvas: bool = False
     size: Tuple[int, int] = (600, 800)
     show: bool = False
-    bg_color: str = "#000000"
+    bg_color: Optional[np.ndarray] = None
