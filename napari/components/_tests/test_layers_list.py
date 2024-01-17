d9456cdc343fceff7224f6c0281fedd8e69faa5e
wmv_hpomen <w-mv@hotmail.com>
2024-01-17 23:29:31 +0100

    remove test

--- napari/components/_tests/test_layers_list.py
+++ napari/components/_tests/test_layers_list.py
@@ -555,7 +555,3 @@ def test_readd_layers():
     with pytest.raises(ValueError):
         layers[:3] = layers[:]
     assert set(layers) == set(imgs)
-
-
-def test_layerlist_axis_labels():
-    "Test that layerlist.axis_labels correspond to the actual labels."
